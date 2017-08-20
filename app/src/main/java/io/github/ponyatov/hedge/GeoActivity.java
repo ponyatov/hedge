c4e793fe4febf84d26d8cdced2bfdc7ad71b01f6
Dmitry Ponyatov <dponyatov@gmail.com>
2017-08-20 22:55:57 +0400

    * geo
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/src/main/java/io/github/ponyatov/hedge/GeoActivity.java
+++ app/src/main/java/io/github/ponyatov/hedge/GeoActivity.java
@@ -8,6 +8,6 @@ public class GeoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.activity_geo);
     }
 }
