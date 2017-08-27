6696b1f2984e92070ea6e0cc70513dfa0f07dd7d
Dmitry Ponyatov <dponyatov@gmail.com>
2017-08-27 11:18:01 +0400

    * etters
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- app/src/main/java/io/github/ponyatov/hedge/Task.java
+++ app/src/main/java/io/github/ponyatov/hedge/Task.java
@@ -1,27 +1,17 @@
 package io.github.ponyatov.hedge;
 
 public class Task {
+
     private boolean mActive;
+    public boolean isActive() { return mActive; }
+    public void setActive(boolean pActive) { mActive = pActive; }
+
     private int mResID;
+    public int getResID() { return mResID; }
+    public void setResID(int pResID) { mResID = pResID; }
 
     public Task(int pResId, boolean pActive) {
-        mResID = pResId;
-        mActive = pActive;
+        setResID(pResId); setActive(pActive);
     }
 
-    public boolean isActive() {
-        return mActive;
-    }
-
-    public void setActive(boolean pActive) {
-        mActive = pActive;
-    }
-
-    public int getResID() {
-        return mResID;
-    }
-
-    public void setResID(int pResID) {
-        mResID = pResID;
-    }
 }
